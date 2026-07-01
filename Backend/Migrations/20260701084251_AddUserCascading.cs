491733e26e007b5ef9697ea678b870f4c2be7153
Moldybot9411 <joewhite8844@gmail.com>
2026-07-01 10:48:56 +0200

    fix: user table cascading

---
+++ Backend/Migrations/20260701084251_AddUserCascading.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserCascading : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Surveys_Folders_FolderId",
+                table: "Surveys");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Surveys_Folders_FolderId",
+                table: "Surveys",
+                column: "FolderId",
+                principalTable: "Folders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Surveys_Folders_FolderId",
+                table: "Surveys");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Surveys_Folders_FolderId",
+                table: "Surveys",
+                column: "FolderId",
+                principalTable: "Folders",
+                principalColumn: "Id");
+        }
+    }
+}
